ed2f50975484c86dbc53981b04ba31495240a3b9
nevermind <nevermind359@gmail.com>
2024-11-18 20:30:31 -0500

    fix: Fix cursor dragging issue

--- script.js
+++ script.js
@@ -1,56 +1,68 @@
-const container = document.querySelector('.container');
-const btn = document.querySelector('.btn');
-const clear = document.querySelector('.clear');
+const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
+const clear = document.querySelector(".clear");
 const containerHeight = 500;
 const containerWidth = 500;
-const mode = document.querySelector('.mode');
+const mode = document.querySelector(".mode");
 
 function getRandNum() {
-    return Math.floor(Math.random() * 255);
+  return Math.floor(Math.random() * 255);
 }
 
-mode.addEventListener('click', function() {
-    if (mode.textContent === 'Rainbow mode') {
-        mode.textContent = 'Black mode';
-    } else {
-        mode.textContent = 'Rainbow mode';
-    }
+let draw = false;
+document.body.addEventListener("mousedown", function () {
+  draw = true;
+});
+document.body.addEventListener("mouseup", function () {
+  draw = false;
+});
+
+mode.addEventListener("click", function () {
+  if (mode.textContent === "Rainbow mode") {
+    mode.textContent = "Black mode";
+  } else {
+    mode.textContent = "Rainbow mode";
+  }
 });
 
 function grid(num) {
-    let width = containerWidth / num;
-    let height = containerHeight / num;
-    console.log(height);
-    for (let i = 0; i < (num*num); i++) {
-        const div = document.createElement('div');
-        div.style.height = `${height}px`;
-        div.style.width = `${width}px`;
-        div.classList.add('grid-item');
-        container.appendChild(div);
-        clear.addEventListener('click', function() {
-            div.style.backgroundColor = 'whitesmoke';
-        })
-        div.addEventListener('mouseover', function() {
-            if (mode.textContent === 'Black mode') {
-                div.style.backgroundColor = `rgb(${getRandNum()}, ${getRandNum()}, ${getRandNum()})`;
-            } else {
-                div.style.backgroundColor = 'black';
-            }
-        });
-    }
+  let width = containerWidth / num;
+  let height = containerHeight / num;
+  console.log(height);
+  for (let i = 0; i < num * num; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("draggable", "false");
+    container.setAttribute("draggable", "false");
+    div.style.height = `${height}px`;
+    div.style.width = `${width}px`;
+    div.classList.add("grid-item");
+    container.appendChild(div);
+    clear.addEventListener("click", function () {
+      div.style.backgroundColor = "whitesmoke";
+    });
+    div.addEventListener("mousemove", function () {
+      if (draw) {
+        if (mode.textContent === "Black mode") {
+          div.style.backgroundColor = `rgb(${getRandNum()}, ${getRandNum()}, ${getRandNum()})`;
+        } else {
+          div.style.backgroundColor = "black";
+        }
+      }
+    });
+  }
 }
 
-btn.addEventListener('click', function userInput() {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-    let choice = prompt('Please, enter the canvas size: from 1 to 100');
-    userChoice = parseInt(choice);    
-    if (userChoice > 0 && userChoice <= 100) {
-        grid(userChoice);
-    } else {
-        return userInput();
-    }
+btn.addEventListener("click", function userInput() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  let choice = prompt("Please, enter the canvas size: from 1 to 100");
+  userChoice = parseInt(choice);
+  if (userChoice > 0 && userChoice <= 100) {
+    grid(userChoice);
+  } else {
+    return userInput();
+  }
 });
 
 grid(10);
