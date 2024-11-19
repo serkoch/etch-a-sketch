94ebadfc572261f6ccbc61a6bda9096ba9ddd4b8
nevermind <nevermind359@gmail.com>
2024-11-19 14:17:51 -0500

    BREAKING CHANGE: Add eraser & fix color change
    Added erase button, updated button layout, separated color button from rainbow button, updated GUI: current mode is indicated on the button & some minor design changes.

--- script.js
+++ script.js
@@ -1,63 +1,85 @@
-const container = document.querySelector(".container");
-const btn = document.querySelector(".btn");
-const clear = document.querySelector(".clear");
+const container = document.querySelector('.container');
+const btn = document.querySelector('.btn');
+const clear = document.querySelector('.clear');
 const containerHeight = 500;
 const containerWidth = 500;
-const mode = document.querySelector(".mode");
+const rainbowMode = document.querySelector('.rainbowMode');
+const colorMode = document.querySelector('.colorMode');
+const eraserMode = document.querySelector('.eraserMode');
 const colorPicker = document.querySelector('.colorPicker');
+
+let bgColor = colorPicker.value;
+let mode = 'Color';
+
+colorMode.addEventListener('click', function () {
+  mode = colorMode.textContent;
+  colorMode.classList.add('active');
+  rainbowMode.classList.remove('active');
+  eraserMode.classList.remove('active');
+});
+rainbowMode.addEventListener('click', function () {
+  mode = rainbowMode.textContent;
+  rainbowMode.classList.add('active');
+  colorMode.classList.remove('active');
+  eraserMode.classList.remove('active');
+});
+eraserMode.addEventListener('click', function () {
+  mode = eraserMode.textContent;
+  eraserMode.classList.add('active');
+  rainbowMode.classList.remove('active');
+  colorMode.classList.remove('active');
+});
+
+function getColor() {
+  if (mode === 'Color') {
+    bgColor = colorPicker.value;
+  } else if (mode === 'Rainbow') {
+    bgColor = `rgb(${getRandNum()}, ${getRandNum()}, ${getRandNum()})`;
+  } else if (mode === 'Eraser') {
+    bgColor = 'whitesmoke';
+  }
+}
 
 function getRandNum() {
   return Math.floor(Math.random() * 255);
 }
 
 let draw = false;
-document.body.addEventListener("mousedown", function () {
+document.body.addEventListener('mousedown', function () {
   draw = true;
 });
-document.body.addEventListener("mouseup", function () {
+document.body.addEventListener('mouseup', function () {
   draw = false;
-});
-
-mode.addEventListener("click", function () {
-  if (mode.textContent === "Rainbow mode") {
-    mode.textContent = "Color mode";
-  } else {
-    mode.textContent = "Rainbow mode";
-  }
 });
 
 function grid(num) {
   let width = containerWidth / num;
   let height = containerHeight / num;
-  console.log(height);
   for (let i = 0; i < num * num; i++) {
-    const div = document.createElement("div");
-    div.setAttribute("draggable", "false");
-    container.setAttribute("draggable", "false");
+    const div = document.createElement('div');
+    div.setAttribute('draggable', 'false');
+    container.setAttribute('draggable', 'false');
     div.style.height = `${height}px`;
     div.style.width = `${width}px`;
-    div.classList.add("grid-item");
+    div.classList.add('grid-item');
     container.appendChild(div);
-    clear.addEventListener("click", function () {
-      div.style.backgroundColor = "whitesmoke";
+    clear.addEventListener('click', function () {
+      div.style.backgroundColor = 'whitesmoke';
     });
-    div.addEventListener("mousemove", function () {
+    div.addEventListener('mousemove', function () {
+      getColor();
       if (draw) {
-        if (mode.textContent === "Color mode") {
-          div.style.backgroundColor = `rgb(${getRandNum()}, ${getRandNum()}, ${getRandNum()})`;
-        } else {
-          div.style.backgroundColor = colorPicker.value;
-        }
+        div.style.backgroundColor = bgColor;
       }
     });
   }
 }
 
-btn.addEventListener("click", function userInput() {
+btn.addEventListener('click', function userInput() {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
-  let choice = prompt("Please, enter the canvas size: from 1 to 100");
+  let choice = prompt('Please, enter the canvas size: from 1 to 100');
   userChoice = parseInt(choice);
   if (userChoice > 0 && userChoice <= 100) {
     grid(userChoice);
